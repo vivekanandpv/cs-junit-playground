dc51672ccc3ef4d657e4e3dc7abc4d11d35295e6
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-13 16:04:17 +0530

    string representation of pairs

--- src/test/java/com/example/junitplayground/services/MathUtilsTest.java
+++ src/test/java/com/example/junitplayground/services/MathUtilsTest.java
@@ -1,12 +1,12 @@
 package com.example.junitplayground.services;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,10 +14,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("MathUtils should")
 class MathUtilsTest {
 
+    @BeforeAll
+    static void setUpTheSuite() {
+        System.out.println("BeforeAll");
+    }
+
+    @AfterAll
+    static void tearDownTheSuite() {
+        System.out.println("AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("AfterEach");
+    }
+
     @DisplayName("compute the square of any number")
     @ParameterizedTest
     @MethodSource("pairStreamProvider")
     void computeSquareOfANumber(NumberSquarePair pair) {
+
+        System.out.println("computeSquareOfANumber >> " + pair);
         //  AAA: Arrange, Act, Assert
         //  Arrange
         MathUtils utils = new MathUtils();
